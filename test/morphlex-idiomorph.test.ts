c39b2c39c08a8f40f20832c5f50f666d9a2e7eba
Joel Drapper <j.drapper@plane.com>
2025-10-31 21:29:53 +0000

    Add idiomorph and morphdom licenses to the tests inspired by them.

--- test/morphlex-idiomorph.test.ts
+++ test/morphlex-idiomorph.test.ts
@@ -1,3 +1,22 @@
+/*
+ * These tests were inspired by idiomorph.
+ * Here's their license:
+ *
+ * Zero-Clause BSD
+ * =============
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 import { describe, it, expect, beforeEach, afterEach } from "vitest"
 import { morph, morphInner } from "../src/morphlex"
 
--- test/morphlex-morphdom.test.ts
+++ test/morphlex-morphdom.test.ts
@@ -1,3 +1,30 @@
+/*
+ * These tests were inspired by morphdom.
+ * Here's their license:
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) Patrick Steele-Idem <pnidem@gmail.com> (psteeleidem.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 import { describe, it, expect, beforeEach, afterEach } from "vitest"
 import { morph } from "../src/morphlex"
 
@@ -556,7 +583,7 @@ describe("Morphdom-style fixture tests", () => {
 			const from = parseHTML(fromHTML)
 			const to = parseHTML(toHTML)
 
-			const originalElements = Array.from(from.children).map(el => el)
+			const originalElements = Array.from(from.children).map((el) => el)
 
 			morph(from, to)
 
