65b14e86721cce7f23ec765305d322da28830273
Joel Drapper <j.drapper@plane.com>
2025-11-21 11:29:02 +0000

    remove data star morph from build script

--- build.ts
+++ build.ts
@@ -5,7 +5,7 @@ import { statSync, readFileSync } from "fs"
 
 // Build and minify with Bun
 await build({
-	entrypoints: ["./src/morphlex.ts", "./src/data-morph.ts"],
+	entrypoints: ["./src/morphlex.ts"],
 	outdir: "./dist",
 	minify: true,
 	sourcemap: "external",
